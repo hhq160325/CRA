ff7a00baaeed00baf4c89f6a7e4723c8388f30b9
Zahava99 <97643126+Zahava99@users.noreply.github.com>
2025-10-14 15:10:32 +0700

    Add CarRental UI
    Introduces CarRental, CarCard, and CarFilters components for the car rental page.

--- src/features/cars/index.js
+++ src/features/cars/index.js
@@ -2,6 +2,9 @@ export { default as RegisterCar } from './components/RegisterCar';
 export { default as RegisterCarStep2 } from './components/RegisterCarStep2';
 export { default as RegisterCarStep3 } from './components/RegisterCarStep3';
 export { default as CarDetail } from './components/CarDetail';
+export { default as CarRental } from './CarRental';
+export { default as CarCard } from './CarCard';
+export { default as CarFilters } from './CarFilters';
 
 // Redux slice exports
 export { default as carsReducer } from './carsSlice';
